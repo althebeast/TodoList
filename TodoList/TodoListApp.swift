672d72720239aa3a7d907c0987b1c6ccea5dd6b0
Alperen Sarışan <88689564+althebeast@users.noreply.github.com>
2024-09-10 21:23:32 +0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Alperen Sarışan on 10.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
